cfb125b108ca3f358f7372e18a04c0379b03bca3
Jose Rivera <josemrivera@gmail.com>
2019-06-23 08:16:53 +0100

    First version of dircrawler.

---
+++ dircrawler.js
@@ -0,0 +1,26 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const entrypoint = "ABS_PATH";
+
+const walk = async (dir, filelist = []) => {
+  const files = await fs.readdir(dir);
+
+  for (file of files) {
+    const filepath = path.join(dir, file);
+    const stat = await fs.stat(filepath);
+
+    if (stat.isDirectory()) {
+      filelist.push(file);
+      const absPath = path.join(dir, file);
+      console.log(absPath.split(entrypoint).pop());
+      filelist = await walk(filepath, filelist);
+    }
+  }
+
+  return filelist;
+};
+
+const filelist = walk(entrypoint);
+
+console.log(filelist);
